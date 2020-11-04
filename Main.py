115464c5641796c836833a934e7b8c9fd7985659
colakamornik <cola.isr@gmail.com>
2020-11-04 21:52:33 +0200

    coloring potential candidates

--- Main.py
+++ Main.py
@@ -359,12 +359,16 @@ Updates Candidates table
                 self.tCandidates.setItem(line, 2, QTableWidgetItem(str(v['Close'])))
                 self.tCandidates.setItem(line, 3, QTableWidgetItem(str(v['Bid'])))
                 self.tCandidates.setItem(line, 4, QTableWidgetItem(str(v['Ask'])))
+                if v['Ask']<v['target_price']:
+                    self.tCandidates.item(line, 4).setBackground(QtGui.QColor(0, 255, 0))
                 if v['target_price'] is float:
                     self.tCandidates.setItem(line, 5, QTableWidgetItem(str(round(v['target_price'], 2))))
                 else:
                     self.tCandidates.setItem(line, 5, QTableWidgetItem(str(v['target_price'])))
                 self.tCandidates.setItem(line, 6, QTableWidgetItem(str(round(v['averagePriceDropP'], 2))))
                 self.tCandidates.setItem(line, 7, QTableWidgetItem(str(v['tipranksRank'])))
+                if int(v['tipranksRank'])>7:
+                    self.tCandidates.item(line, 7).setBackground(QtGui.QColor(0, 255, 0))
                 self.tCandidates.setItem(line, 8, QTableWidgetItem(str(v['LastUpdate'])))
 
                 line += 1
