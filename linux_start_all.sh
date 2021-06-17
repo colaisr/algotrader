04157a0dc32a36fbfad66cd8e9bc24f76d509d79
colakamornik <cola.isr@gmail.com>
2021-06-17 13:57:23 +0300

    linux startinfg

--- linux_start_all.sh
+++ linux_start_all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo chmod +x Scripts\linux_twsRestartScript.sh
-Scripts\linux_twsRestartScript.sh
+sudo chmod +x Scripts/linux_twsRestartScript.sh
+Scripts/linux_twsRestartScript.sh
 python3 Main.py
