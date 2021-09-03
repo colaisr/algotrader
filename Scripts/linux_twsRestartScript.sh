79d00837cc6c43d124174e8595da5f870240593b
colakamornik <cola.isr@gmail.com>
2021-09-03 14:36:37 +0300

    script shortened

--- Scripts/linux_twsRestartScript.sh
+++ Scripts/linux_twsRestartScript.sh
@@ -5,7 +5,7 @@ sleep 1
 echo "Restarting TWS"
 /home/su/Jts/ibgateway/981/ibgateway start&
 echo "TWS started"
-sleep 10
+sleep 30
 echo "finished waiting"
 python3 Scripts/tws_cred_login.py
 echo "Finished login to TWS"
