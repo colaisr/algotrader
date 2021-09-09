64b9f00477f872b70975b8948de5fd03311fc937
colakamornik <cola.isr@gmail.com>
2021-09-09 16:59:18 +0300

    sleep enlarged

--- Scripts/linux_twsRestartScript.sh
+++ Scripts/linux_twsRestartScript.sh
@@ -5,7 +5,7 @@ sleep 1
 echo "Restarting TWS"
 /home/su/Jts/ibgateway/981/ibgateway start&
 echo "TWS started"
-sleep 30
+sleep 50
 echo "finished waiting"
 python3 Scripts/tws_cred_login.py
 echo "Finished login to TWS"
